f55dfbbf6f7bada1d373d57e0f31ff918da09db9
Simon <simjenner3@gmail.com>
2018-05-13 18:15:49 +0100

    Fixed DBL stats updater

--- source/dblTask.py
+++ source/dblTask.py
@@ -4,20 +4,35 @@ Handles interactions with the discordbots.org API
 
 from os import environ
 import asyncio
+import aiohttp
 import utils
-import dbl
+
+dblAPI = "https://discordbots.org/api/bots/{}/stats"
 
 try:
     dblToken = environ["dblToken"]
 except KeyError:
     utils.err("Environment Variable \"dblToken\" cannot be found")
 
+dblHeaders = {"Authorization": dblToken, "Content-Type": "application/json"}
+
 async def dblBackgroundTask(clientObject):
+
+    dblUpdateURL = dblAPI.format(clientObject.user.id)
     await clientObject.wait_until_ready()
-    dblpy = dbl.Client(clientObject, dblToken)
-    while not clientObject.is_closed:
+
+    while not client.is_closed:
+        # POST Stats
+        serverCount = len(list(clientObject.servers))
+
         try:
-            await dblpy.post_server_count()
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                    dblUpdateURL,
+                    json={"server_count": serverCount},
+                    headers=dblHeaders
+                )
         except Exception as e:
             print("[dbl] Failed to post server count\n{}: {}".format(type(e).__name__, e))
+
         await asyncio.sleep(1800)
