72734c290f6d750c5409b689f07d035a2e277d7e
Simon <simjenner3@gmail.com>
2018-08-25 17:10:12 +0100

    Edit script

--- scripts/updateGit.sh
+++ scripts/updateGit.sh
@@ -1,5 +1,7 @@
 # Pull latest updates from git repo
 # For some reason it doesen't like just `git pull`
 # sudo because file permissions
+# chown because "sudo git" changes owner
 sudo git fetch --all
 sudo git reset --hard origin/master
+sudo chown -R SLB /opt/SpaceX-Launch-Bot
