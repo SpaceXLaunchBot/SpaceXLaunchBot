98d03ff84a04373af899147a2969b0e60cf2b378
Simon <simjenner3@gmail.com>
2018-09-13 19:50:02 +0100

    Added check for config keys

--- source/modules/fs.py
+++ source/modules/fs.py
@@ -11,9 +11,15 @@ Load local config file and create a dictionary that can be accessed by everythin
 that imports this module
 """
 configFilePath = path.join(path.dirname(path.abspath(__file__)), "..", "config", "config.json")
-# TODO: Check for needed keys
+neededKeys =["commandPrefix", "apiCheckInterval", "launchNotificationDelta", "reaperInterval", "logFormat"]
+
 try:
     with open(configFilePath, "r") as inFile:
         config = json.load(inFile)
+    
+    for key in neededKeys:
+        if key not in config:
+            fatalError(f"Cannot find required key: {key} in configuration file")
+
 except FileNotFoundError:
     fatalError("Configuration file / directory not found")
