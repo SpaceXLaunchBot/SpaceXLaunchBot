72d23399f79364be18e9115308132f4efbd0a0de
thatguywiththatname <simjenner3@gmail.com>
2019-05-16 20:15:36 +0100

    rename files

---
+++ source/modules/bg_tasks.py
@@ -0,0 +1,66 @@
+"""BG Task algo (WIP):
+Get new embed dict
+--> has it changed?
+-->--y send all subbed channels updated embed
+-->--n n/a
+--> do we meet time criteria for launch notifs?
+-->--y have we already sent a launch notif for this embed?
+-->--y--n send launch notif
+-->--y--y n/a
+-->--n n/a
+--> sleep for ONE_MINUTE?
+"""
+
+import logging, asyncio
+
+import config
+from modules import apis, embed_generators
+from modules.redis_client import redis
+
+logger = logging.getLogger(__name__)
+
+ONE_MINUTE = 60
+
+
+async def notificationTask(client):
+    await client.wait_until_ready()
+    while not client.is_closed():
+
+        # At the end of this method, remove all channels that we can't access anymore
+        channelsToRemove = []
+
+        next_launch_dict = await apis.SpacexApi.get_next_launch_dict()
+
+        if next_launch_dict != -1:
+
+            launch_info_embed = await embed_generators.gen_launch_info_embeds(
+                next_launch_dict
+            )
+            launch_info_embed_dict = launch_info_embed.to_dict()
+
+            subbedChannelIDs = await redis.smembers("slb.subscribed_channels")
+            subbedChannelIDs = (int(cid) for cid in subbedChannelIDs)
+
+            notifTaskStore = await redis.get_notification_task_store()
+            launching_soon_notif_sent = notifTaskStore["launching_soon_notif_sent"]
+            latest_launch_info_embed_dict = notifTaskStore[
+                "latest_launch_info_embed_dict"
+            ]
+
+            if launch_info_embed_dict != latest_launch_info_embed_dict:
+                logger.info("Launch info changed, sending notifications")
+
+                launching_soon_notif_sent = "False"
+                latest_launch_info_embed_dict = launch_info_embed_dict
+
+                # New launch found, send all "subscribed" channels the embed
+                for channelID in subbedChannelIDs:
+                    channel = client.get_channel(channelID)
+                    if channel == None:
+                        channelsToRemove.append(channelID)
+                    else:
+                        await client.safe_send(channel, launch_info_embed)
+
+        # TODO: Notifications for "launching soon"
+
+        await asyncio.sleep(ONE_MINUTE * config.API_CHECK_INTERVAL)
---
+++ source/modules/embed_generators.py
@@ -0,0 +1,171 @@
+from copy import deepcopy
+from discord import Embed
+
+from modules.structure import utc_from_ts
+from modules.statics import (
+    falcon_red,
+    rocket_id_images,
+    general_error_embed,
+    owner_mention,
+)
+
+payload_info = """Type: {}
+Orbit: {}
+Mass: {}
+Manufacturer: {}
+Customer{}: {}
+"""
+core_info = """Serial: {}
+Flight: {}
+Landing: {}
+Landing Type: {}
+Landing Vehicle: {}
+"""
+launch_date_info = """{}
+Precision: {}
+"""
+
+
+async def gen_launch_info_embeds(next_launch_dict):
+
+    # Having desc set to `None` breaks things
+    if next_launch_dict["details"] == None:
+        next_launch_dict["details"] = ""
+
+    launch_info_embed = Embed(
+        color=falcon_red,
+        description=next_launch_dict["details"],
+        title="Launch #{} - {}".format(
+            next_launch_dict["flight_number"], next_launch_dict["mission_name"]
+        ),
+    )
+
+    # Set thumbnail depending on rocket ID
+    if next_launch_dict["rocket"]["rocket_id"] in rocket_id_images:
+        launch_info_embed.set_thumbnail(
+            url=rocket_id_images[next_launch_dict["rocket"]["rocket_id"]]
+        )
+
+    launch_info_embed.add_field(
+        name="Launch vehicle",
+        value="{} {}".format(
+            next_launch_dict["rocket"]["rocket_name"],
+            next_launch_dict["rocket"]["rocket_type"],
+        ),
+    )
+
+    # Add a field for the launch date
+    UTCLaunchDate = await utc_from_ts(next_launch_dict["launch_date_unix"])
+    launch_info_embed.add_field(
+        name="Launch date",
+        value=launch_date_info.format(
+            UTCLaunchDate, next_launch_dict["tentative_max_precision"]
+        ),
+    )
+
+    # Basic embed structure built, copy into small version
+    launch_info_embedSmall = deepcopy(launch_info_embed)
+
+    discussionURL = next_launch_dict["links"]["reddit_campaign"]
+    if discussionURL != None:
+        launch_info_embed.add_field(name="r/SpaceX discussion", value=discussionURL)
+
+    launch_info_embed.add_field(
+        name="Launch site", value=next_launch_dict["launch_site"]["site_name_long"]
+    )
+
+    if next_launch_dict["rocket"]["rocket_id"] == "falcon9":
+        # TODO: ALlow "Core Info" section for FH
+        # Falcon 9 always has 1 core, FH (or others) will be different
+        launch_info_embed.add_field(
+            name="Core info",
+            value=core_info.format(
+                next_launch_dict["rocket"]["first_stage"]["cores"][0]["core_serial"],
+                next_launch_dict["rocket"]["first_stage"]["cores"][0]["flight"],
+                next_launch_dict["rocket"]["first_stage"]["cores"][0]["landing_intent"],
+                next_launch_dict["rocket"]["first_stage"]["cores"][0]["landing_type"],
+                next_launch_dict["rocket"]["first_stage"]["cores"][0][
+                    "landing_vehicle"
+                ],
+            ),
+        )
+
+    # Add a field for each payload, with basic information
+    for payload in next_launch_dict["rocket"]["second_stage"]["payloads"]:
+        launch_info_embed.add_field(
+            name="Payload: {}".format(payload["payload_id"]),
+            value=payload_info.format(
+                payload["payload_type"],
+                payload["orbit"],
+                payload["payload_mass_kg"],
+                payload["manufacturer"],
+                "(s)" if len(payload["customers"]) > 1 else "",
+                ", ".join(payload["customers"]),
+            ),
+        )
+
+    if len(launch_info_embed.title) > 256:
+        # Title too big to send, no way around this other than send an err
+        return general_error_embed
+    elif len(launch_info_embed) > 2048:
+        # If body is too big, send small embed
+        return launch_info_embedSmall
+    return launch_info_embed
+
+
+async def genLaunchingSoonEmbed(next_launch_dict):
+    embedDesc = ""
+
+    notifEmbed = Embed(
+        color=falcon_red,
+        title="{} is launching soon!".format(next_launch_dict["mission_name"]),
+    )
+
+    if next_launch_dict["links"]["mission_patch_small"] != None:
+        notifEmbed.set_thumbnail(url=next_launch_dict["links"]["mission_patch_small"])
+    elif next_launch_dict["rocket"]["rocket_id"] in rocket_id_images:
+        notifEmbed.set_thumbnail(
+            url=rocket_id_images[next_launch_dict["rocket"]["rocket_id"]]
+        )
+
+    # Embed links [using](markdown)
+    if next_launch_dict["links"]["video_link"] != None:
+        embedDesc += f"[Livestream]({next_launch_dict['links']['video_link']})\n"
+    if next_launch_dict["links"]["reddit_launch"] != None:
+        embedDesc += (
+            f"[r/SpaceX Launch Thread]({next_launch_dict['links']['reddit_launch']})\n"
+        )
+    if next_launch_dict["links"]["presskit"] != None:
+        embedDesc += f"[Press kit]({next_launch_dict['links']['presskit']})\n"
+    notifEmbed.description = embedDesc
+
+    UTCLaunchDate = await utc_from_ts(next_launch_dict["launch_date_unix"])
+    notifEmbed.add_field(name="Launch date", value=UTCLaunchDate)
+
+    return notifEmbed
+
+
+async def getInfoEmbed():
+    """Info ideas:
+    - Developer / Owner tag
+    - Guild, Channel, and User count
+    - Uptime
+    - Github link
+    - Invite link
+    - Bot metrics (command count, avg commands per ?, etc.)
+    - API latency?
+    - Let user know about !help
+    """
+    info_embed = Embed(
+        title="SpaceX-Launch-Bot Information",
+        color=falcon_red,
+        description="A Discord bot for getting news, information, and notifications about upcoming SpaceX launches",
+    )
+    info_embed.add_field(
+        name="Links", value="[GitHub](https://github.com/r-spacex/SpaceX-Launch-Bot)"
+    )
+    info_embed.add_field(name="Contact", value=f"{owner_mention}")
+    info_embed.add_field(
+        name="Help", value="Use the command !help to get a list of commands"
+    )
+    return info_embed
