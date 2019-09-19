ed454d20280d0d34c8e63473bb4d728d2f020970
thatguywiththatname <simjenner3@gmail.com>
2019-09-19 14:27:13 +0100

    Test influx config

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,6 +7,10 @@ def test_redis_config():
     assert isinstance(config.REDIS_PORT, int)
 
 
+def test_influx_config():
+    assert isinstance(config.INFLUX_DB, str)
+
+
 def test_notif_task_config():
     assert isinstance(config.NOTIF_TASK_API_INTERVAL, int)
     assert isinstance(config.NOTIF_TASK_LAUNCH_DELTA, int)
