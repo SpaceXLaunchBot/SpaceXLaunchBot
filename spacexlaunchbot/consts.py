01ea75f25e4444aeed202a00906f92650f575f6b
thatguywiththatname <simjenner3@gmail.com>
2020-08-13 03:49:25 +0100

    Thanks Vulture

--- spacexlaunchbot/consts.py
+++ spacexlaunchbot/consts.py
@@ -7,7 +7,7 @@ class Colour:
     # pylint: disable=too-few-public-methods
     red_error = Color.from_rgb(255, 0, 0)
     red_falcon = Color.from_rgb(238, 15, 70)
-    orange_info = Color.from_rgb(200, 74, 0)
+    # orange_info = Color.from_rgb(200, 74, 0)
 
 
 class NotificationType(Enum):
