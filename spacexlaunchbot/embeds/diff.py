cd9502d7710e123dfac13ab0acac1619b04b8679
thatguywiththatname <simjenner3@gmail.com>
2021-09-19 17:10:06 +0100

    Quick hack to fix falcon heavy launch causing notification spam

--- spacexlaunchbot/embeds/diff.py
+++ spacexlaunchbot/embeds/diff.py
@@ -20,12 +20,26 @@ def diff_schedule_embed_dicts(old_embed: dict, new_embed: dict) -> str:
         field["name"]: field["value"] for field in old_embed.get("fields", [])
     }
 
+    new_embed_fields = new_embed.get("fields", [])
+
+    # FIXME: Quick hack to get launch vehicle.
+    launch_vehicle = ""
+    for f in new_embed_fields:
+        if f["name"] == "Launch Vehicle":
+            launch_vehicle = f["value"]
+
     # This detects all field changes except if old_embed has a field that new_embed
     # does not (e.g. a payload was removed). Not going to worry about this for now as
     # it's unlikely to happen.
     # TODO: Detect field removals.
-    for field in new_embed.get("fields", []):
+    for field in new_embed_fields:
         name = field["name"]
+
+        # FIXME: We have to skip Core Info for Falcon Heavys as they have 3 Core Info
+        #  fields all with the same name, which breaks this comparison that we're doing.
+        if launch_vehicle == "Falcon Heavy rocket" and field["name"] == "Core Info":
+            continue
+
         if name in old_embed_fields:
             if old_embed_fields[name] != field["value"]:
                 diffs += [name]
